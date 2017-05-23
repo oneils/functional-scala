eaa08d3afdd4dbb2f99917c9861657295b19f4f1
Aliaksei <lex.nox@gmail.com>
2017-05-23 12:25:43 +0300

    WIP Chapter-4 Option example is added.

--- src/main/scala/info/devpages/chapter4/Option.scala
+++ src/main/scala/info/devpages/chapter4/Option.scala
@@ -36,13 +36,39 @@ sealed trait Option[+A] {
   /**
     * Don’t evaluate ob unless needed.
     */
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(_) => ob
+  }
 
   /**
     * Convert Some to None if the value doesn’t satisfy f .
     */
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => Some(a)
+    case _ => None
+  }
 
+  /**
+    * Implementing filter by using flatmap
+    */
+  def filter1(f: A => Boolean): Option[A] = {
+    flatMap(a => if (f(a)) Some(a) else None)
+  }
+
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+  /**
+    * combines a list of Options into one Option containing
+    * a list of all the Some values in the original list. If the original list contains None even
+    * once, the result of the function should be None; otherwise the result should be Some
+    * with a list of all the values
+    *
+    */
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => None
+    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+  }
 }
 
 case class Some[A](get: A) extends Option[A]
